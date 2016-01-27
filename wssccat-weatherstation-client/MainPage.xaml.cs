f67fe12483f19ee12076c12fb949e604a95656e1
Chris Segura <chris.segura@live.com>
2016-01-26 21:11:54 -0800

    update with added line chart display and random number generator for chart values

--- wssccat-weatherstation-client/MainPage.xaml.cs
+++ wssccat-weatherstation-client/MainPage.xaml.cs
@@ -16,6 +16,7 @@ using WinRTXamlToolkit.Controls;
 using WinRTXamlToolkit.Controls.DataVisualization;
 using WinRTXamlToolkit.Controls.Common;
 using WinRTXamlToolkit.Controls.Extensions;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,6 +30,35 @@ namespace wssccat_weatherstation_client
         public MainPage()
         {
             this.InitializeComponent();
+            UpdateCharts();
+        }
+        private Random _random = new Random();
+
+        private void UpdateCharts()
+        {
+            List<NameValueItem> items = new List<NameValueItem>();
+            items.Add(new NameValueItem { Name = "Test1", Value = _random.Next(10, 100) });
+            items.Add(new NameValueItem { Name = "Test2", Value = _random.Next(10, 100) });
+            items.Add(new NameValueItem { Name = "Test3", Value = _random.Next(10, 100) });
+            items.Add(new NameValueItem { Name = "Test4", Value = _random.Next(10, 100) });
+            items.Add(new NameValueItem { Name = "Test5", Value = _random.Next(10, 100) });
+
+            ((LineSeries)LineChartWithAxes.Series[0]).ItemsSource = items;
+            ((LineSeries)LineChartWithAxes.Series[0]).DependentRangeAxis =
+                new LinearAxis
+                {
+                    Minimum = 0,
+                    Maximum = 100,
+                    Orientation = AxisOrientation.Y,
+                    Interval = 20,
+                    ShowGridLines = true
+                };
+        }
+
+        public class NameValueItem
+        {
+            public string Name { get; set; }
+            public int Value { get; set; }
         }
     }
 }
