d39bab9af49683b1a767a1c49d616cdb6f65ade1
Chris Segura <chris.segura@live.com>
2016-01-27 15:07:22 -0800

    update with partially working line graph

--- wssccat-weatherstation-client/MainPage.xaml.cs
+++ wssccat-weatherstation-client/MainPage.xaml.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
 using WinRTXamlToolkit.Controls;
 using WinRTXamlToolkit.Controls.DataVisualization;
 using WinRTXamlToolkit.Controls.Common;
@@ -27,38 +33,112 @@ namespace wssccat_weatherstation_client
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SensorData data = new SensorData();
+        private HttpClient sensorClient;
+        private HttpBaseProtocolFilter sensorFilter = new HttpBaseProtocolFilter();
+        private Random _random = new Random();
+        private Uri sensorUri = new Uri("http://wssccat:1038");
         public MainPage()
         {
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
             this.InitializeComponent();
-            UpdateCharts();
-        }
-        private Random _random = new Random();
+            
+            //Create timeer based ThreadPool task to get data from sensors and update UI
+            ThreadPoolTimer readerTimer = ThreadPoolTimer.CreatePeriodicTimer(async (source) =>
+            {
+                await ClearScreen();
+                GetData();
 
-        private void UpdateCharts()
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => UpdateScreen());
+            }, TimeSpan.FromSeconds(2));
+            
+        }
+
+
+        private async Task ClearScreen(CoreDispatcherPriority priority = CoreDispatcherPriority.Low)
+        {
+            await Dispatcher.RunAsync(priority, () => { Status.Text = ""; });
+        }
+        async void GetData()
+        {
+            try
+            {
+                //using (HttpResponseMessage response = await sensorClient.GetAsync(sensorUri))
+                bool TEMP = false;
+                {
+                    if (TEMP == true)
+                        //(response.IsSuccessStatusCode)
+                    {
+                        //TODO PARSE JSON ETC.
+                    }
+                    else
+                    {
+                        data.FahrenheitTemperature = _random.Next(45, 100);
+                        LogToScreen("Fake temp is: " + data.FahrenheitTemperature);
+                        /*
+                        List<NameValueItem> items = new List<NameValueItem>();
+                        items.Add(new NameValueItem { Name = "Test1", Value = _random.Next(10, 100) });
+                        items.Add(new NameValueItem { Name = "Test2", Value = _random.Next(10, 100) });
+                        items.Add(new NameValueItem { Name = "Test3", Value = _random.Next(10, 100) });
+                        items.Add(new NameValueItem { Name = "Test4", Value = _random.Next(10, 100) });
+                        items.Add(new NameValueItem { Name = "Test5", Value = _random.Next(10, 100) });
+                        
+                        ((LineSeries)LineChartWithAxes.Series[0]).ItemsSource = items;
+                        ((LineSeries)LineChartWithAxes.Series[0]).DependentRangeAxis =
+                            new LinearAxis
+                            {
+                                Minimum = 0,
+                                Maximum = 100,
+                                Orientation = AxisOrientation.Y,
+                                Interval = 20,
+                                ShowGridLines = true
+                            };
+                            */
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO LOG TO SCREEN
+            }
+            finally
+            {
+                //TODO LOG TO SCREEN
+            }
+        }
+
+        private async void LogToScreen (string text, CoreDispatcherPriority priority = CoreDispatcherPriority.Low)
+        {
+            await Dispatcher.RunAsync(priority, () => { Status.Text += text = "\n"; });
+        }
+        private void UpdateScreen()
         {
             List<NameValueItem> items = new List<NameValueItem>();
-            items.Add(new NameValueItem { Name = "Test1", Value = _random.Next(10, 100) });
-            items.Add(new NameValueItem { Name = "Test2", Value = _random.Next(10, 100) });
-            items.Add(new NameValueItem { Name = "Test3", Value = _random.Next(10, 100) });
-            items.Add(new NameValueItem { Name = "Test4", Value = _random.Next(10, 100) });
-            items.Add(new NameValueItem { Name = "Test5", Value = _random.Next(10, 100) });
-
+            items.Add(new NameValueItem { Name = data.TimeStamp, Value = data.FahrenheitTemperature });
             ((LineSeries)LineChartWithAxes.Series[0]).ItemsSource = items;
-            ((LineSeries)LineChartWithAxes.Series[0]).DependentRangeAxis =
-                new LinearAxis
-                {
-                    Minimum = 0,
-                    Maximum = 100,
-                    Orientation = AxisOrientation.Y,
-                    Interval = 20,
-                    ShowGridLines = true
-                };
-        }
 
+        }
         public class NameValueItem
         {
             public string Name { get; set; }
             public int Value { get; set; }
+        }
+        public sealed class SensorData
+        {
+            public SensorData()
+            {
+                TimeStamp = DateTimeOffset.Now.ToLocalTime().ToString();
+            }
+            public float BarometricPressure { get; set; }
+            public float CelciusTemperature { get; set; }
+            public int FahrenheitTemperature { get; set; } 
+            public float Humidity { get; set; }
+            public string TimeStamp { get; set; }
+        }
+
+        private void Status_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
