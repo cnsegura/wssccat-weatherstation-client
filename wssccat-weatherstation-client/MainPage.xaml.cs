97efda4b8bde3d9f1292fd11d1def5a000b0c2ac
Chris Segura <chris.segura@live.com>
2016-02-11 11:52:36 -0800

    Replaced int/string items where they were commented out for debugging

--- wssccat-weatherstation-client/MainPage.xaml.cs
+++ wssccat-weatherstation-client/MainPage.xaml.cs
@@ -96,13 +96,13 @@ namespace wssccat_weatherstation_client
                     }
                     else
                     {
-                        //data.FahrenheitTemperature = _random.Next(75, 100);
-                        data.FahrenheitTemperature = "88";
+                        data.FahrenheitTemperature = _random.Next(75, 100);
+                        //data.FahrenheitTemperature = "88";
                         data.TimeStamp = DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":" + DateTime.Now.Second.ToString();
-                        //data.BarometricPressure = _random.Next(20, 100);
-                        data.BarometricPressure = "99";
-                        //data.Humidity = _random.Next(20, 100);
-                        data.Humidity = "77";
+                        data.BarometricPressure = _random.Next(20, 100);
+                        //data.BarometricPressure = "99";
+                        data.Humidity = _random.Next(20, 100);
+                        //data.Humidity = "77";
                         return new NameValueItem { Name = data.TimeStamp, Value = data.FahrenheitTemperature };
                        
                     }
@@ -148,23 +148,23 @@ namespace wssccat_weatherstation_client
                         Interval = 20,
                         ShowGridLines = true
                     };
-            //topGauge.Value = data.BarometricPressure;
-            //bottomGauge.Value = data.Humidity;
+            topGauge.Value = data.BarometricPressure;
+            bottomGauge.Value = data.Humidity;
         }
         private async Task PostDataAsync()
         {
             string topicString = "/SensorData";
             UriBuilder u1 = new UriBuilder();
-            //u1.Host = "localhost";
+            //u1.Host = "localhost"; //DEBUG
             u1.Host = "wssccatiot.westus.cloudapp.azure.com";
             u1.Port = 8082;
             u1.Path = "topics" + topicString;
             u1.Scheme = "http";
             Uri topicUri = u1.Uri;
             string jsonBody = JsonConvert.SerializeObject(data, Formatting.None);
-            string correctedJsonBody = jsonBody.Replace(",", "}}, {\"value\":{");
-            string jsonHeader = ("{\"records\":[{\"value\":");
-            string jsonFooter = ("}]}");
+            string correctedJsonBody = jsonBody.Replace(",", "}}, {\"value\":{"); //have to add in some json features into the string. Easier than creating unnecessary classes that would make this come out automatically
+            string jsonHeader = ("{\"records\":[{\"value\":"); //same as above, fixing string for Server requirements
+            string jsonFooter = ("}]}"); //ditto
             string json = jsonHeader + correctedJsonBody + jsonFooter;
             //Currently focused on REST API surface for Confluent.io Kafka deployment. We can make this more generic in the future
             var baseFilter = new HttpBaseProtocolFilter();
@@ -172,8 +172,6 @@ namespace wssccat_weatherstation_client
             HttpClient httpClient = new HttpClient(baseFilter);
             try
             {
-                //httpClient.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/vnd.kafka.json.v1+json")); //replace with accept header
-                //HttpResponseMessage postResponse = await httpClient.PostAsync(topicUri, new HttpStringContent(json, Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/vnd.kafka.json.v1+json"));
                 var headerContent = new HttpStringContent(json);
                 headerContent.Headers.ContentType = null; // removing all header content and will replace with the required values
                 headerContent.Headers.ContentType = new HttpMediaTypeHeaderValue("application/vnd.kafka.json.v1+json"); //Content-Type: application/vnd.kafka.json.v1+json
@@ -189,8 +187,8 @@ namespace wssccat_weatherstation_client
         public class NameValueItem
         {
             public string Name { get; set; }
-            //public int Value { get; set; }
-            public string Value { get; set; }
+            public int Value { get; set; }
+            //public string Value { get; set; }
         }
         public partial class SensorData
         {
@@ -200,17 +198,19 @@ namespace wssccat_weatherstation_client
                 var localName = hostNames.FirstOrDefault(name => name.DisplayName.Contains(".local"));
                 ClientName = localName.DisplayName.Replace(".local", "");
             }
-            //public int BarometricPressure { get; set; }
-            ////public float CelciusTemperature { get; set; }
-            //public int FahrenheitTemperature { get; set; }
-            //public int Humidity { get; set; }
+            public int BarometricPressure { get; set; }
+            //public float CelciusTemperature { get; set; }
+            public int FahrenheitTemperature { get; set; }
+            public int Humidity { get; set; }
             public string TimeStamp { get; set; }
             public string ClientName { get; set; }
 
-            public string BarometricPressure { get; set; }
-            //public string CelciusTermperature { get; set; }
-            public string Humidity { get; set; }
-            public string FahrenheitTemperature { get; set; }
+
+            //DEBUG STRINGS
+            //public string BarometricPressure { get; set; }
+            ////public string CelciusTermperature { get; set; }
+            //public string Humidity { get; set; }
+            //public string FahrenheitTemperature { get; set; }
            
 
 
